ee78f3dccb307b91c4a74979988243c70535c95b
Koray Koska <koray@koska.at>
2019-01-31 15:00:39 +0100

    Debug logs

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -15,5 +15,33 @@ public func routes(_ router: Router, _ customConfigService: CustomConfigService)
     telegramBot.messageUpdate = controller.getMessage
     telegramBot.callbackQueryUpdate = controller.getCallback
 
-    telegramBot.setupWebhook(path: customConfigService.telegramToken, routerFunction: router.telegramRegister)
+    telegramBot.setupWebhook(path: customConfigService.telegramToken, routerFunction: router.telegramRegisterAlt)
+}
+
+extension Router {
+
+    public func telegramRegisterAlt(path: String, callback: @escaping (_ req: TelegramHTTPRequest, _ cb: @escaping (TelegramHTTPStatus) -> ()) -> ()) {
+        post(path) { reqInternal -> Future<HTTPStatus> in
+            let promiseStatus = reqInternal.eventLoop.newPromise(HTTPStatus.self)
+
+            // DEBUG ONLY
+            let logger = try reqInternal.make(Logger.self)
+            logger.error("*** REQUEST ***")
+            let body = reqInternal.http.body.data
+            logger.error(body?.base64EncodedString() ?? "NIL")
+
+            callback(reqInternal) { status in
+                switch status {
+                case .ok:
+                    promiseStatus.succeed(result: .ok)
+                case .badRequest:
+                    promiseStatus.succeed(result: .badRequest)
+                case .internalServerError:
+                    promiseStatus.succeed(result: .internalServerError)
+                }
+            }
+
+            return promiseStatus.futureResult
+        }
+    }
 }
